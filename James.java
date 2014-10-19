ddae5b3083c3350a42c913d3d4dc1a8364467a11
Jack Reuter <jackreuter@Jacks-MacBook-Air.local>
2014-10-19 18:02:18 +0200

    Jamesy

--- James.java
+++ James.java
@@ -1,3 +1,77 @@
+<<<<<<< HEAD
+public James extends Shooter{
+	
+	public String play(String mine, String other) {
+		if(mine.equals("")) {return "R";}
+		if(mine.length() == 1) {return "B";}
+		int otherAmmo = 0;
+		int myAmmo = 0;
+		for (int i = 0; i < other.length();i++) 
+		{
+			if(other.charAt(i) == 'R') {
+				otherAmmo++;
+			}
+			if(other.charAt(i) == 'S') {
+				otherAmmo--;
+			}
+		}
+		for (int i = 0; i < mine.length();i++) 
+		{
+			if(mine.charAt(i) == 'R') {
+				myAmmo++;
+			}
+			if(other.charAt(i) == 'S') {
+				myAmmo--;
+			}
+		}
+		if(otherAmmo >0 && otherAmmo < 5) {
+			return "B";
+		}
+		if(otherAmmo >= 5) {
+			return "S";
+		}
+		if(otherAmmo < 1) 
+		{
+			if(myAmmo > 0){
+				return "S";
+			} else {
+				return "R";
+			}
+		}
+		if(myAmmo > 8) {
+			return "S";
+		}
+		if(myAmmo < 9 && myAmmo > otherAmmo) {
+			return "S";
+		}
+		char block = 'B';
+		int blocks = 0;
+		if(other.length() > 10) 
+		{
+			int i = other.length() - 1;
+			while (block == 'B') 
+			{
+				if(other.charAt(i) == 'B') {
+					blocks++;
+					block = 'B';
+					i--;
+				} else {
+					block ='C';
+				}
+			}
+		}
+		if(blocks > 7 && myAmmo > 5)
+		{
+			return 'S';
+		}
+		if(blocks > 7) {
+			return 'R';
+		}
+	
+	return "R";
+	}
+}
+=======
 public class James extends Shooter{
 	
 	public String play(String mine, String other) {
@@ -38,3 +112,4 @@ public class James extends Shooter{
     return "R";
 	}
 }
+>>>>>>> a31696d138b51706a088b74f214d840ab023b7c0
