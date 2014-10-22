12b4645447b1e324ee8833d0c9a61423f5f28983
Miles Crabill <miles@milescrabill.com>
2014-10-22 18:20:55 +0200

    minor QuickTournament change

--- QuickTournament.java
+++ QuickTournament.java
@@ -6,25 +6,25 @@ public class QuickTournament {
     
     public static void main(String args[]) {
         int number_of_iterations = 10;
-        if (args.length > 0) {
+        for (int i = 0; i < args.length; i++) {
+            // setting iterations
             try {
-                number_of_iterations = Integer.parseInt(args[0]);
+                number_of_iterations = Integer.parseInt(args[i]);
             } catch (NumberFormatException e) {
-                // in case args[0] is not an int
+                // in case args[i] is not an int
             }
         }
         
-
         FightClub fc = new FightClub("Tournament_Cowboys.txt");
         int number_of_cowboys = fc.cowboys.size();
         for (int n=0; n<number_of_iterations; n++) {
-          for (int i=0; i<number_of_cowboys; i++) {
-            for (int j=0; j<number_of_cowboys; j++) {
-              if (i != j) {
-                fc.fight(fc.cowboys.get(i),fc.cowboys.get(j),false);
-              }
+            for (int i=0; i<number_of_cowboys; i++) {
+                for (int j=0; j<number_of_cowboys; j++) {
+                    if (i != j) {
+                        fc.fight(fc.cowboys.get(i),fc.cowboys.get(j),false);
+                    }
+                }
             }
-          }
         }
         fc.printStandings();
     }
