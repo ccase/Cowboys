102ca762c95ecc9b5c2283510170106714513e29
ccase <ccase16@cmc.edu>
2014-11-20 15:55:38 +0100

    Sacrificing children to Satan...
    For good luck

--- PaleRider.java
+++ PaleRider.java
@@ -1,3 +1,6 @@
+///The Pale Rider Never Misses
+
+
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
@@ -12,7 +15,7 @@ public class PaleRider extends Shooter {
 String opponent = "";
 PrintStream tempOut = System.out;
 
-String pattern = "X";
+String pattern = "RBBBS";
 
 
 public String play(String me, String him){
@@ -41,8 +44,19 @@ public String play(String me, String him){
 	        	if(line.contains("SimpleJoe")){ opponent = "SimpleJoe"; pattern = "RBS"; }
 	        	if(line.contains("AveragePete")){ opponent = "AveragePete"; pattern = "RBBS"; }
 	        	if(line.contains("Sprinter")){ opponent = "Sprinter"; pattern = "RS"; }
-	        	if(line.contains("Elephant")){ opponent = "Elephant"; pattern = "RBBBBBS"; }
-	        	if(line.contains("Kemosabe")){ opponent = "Sprinter"; pattern = "RBBBS"; }
+	        	if(line.contains("Elephant")){ opponent = "Elephant"; pattern = "RBBBS"; }
+	        	if(line.contains("Bojango")){ opponent = "Bojango"; pattern = "RBBBS"; }
+	        	if(line.contains("CamelCase")){ opponent = "CamelCase"; pattern = "RBBBBBS"; }
+	        	if(line.contains("Kemosabe")){ opponent = "Kemosabe"; pattern = "RBBBS"; }
+	        	if(line.contains("CopyCatBitch")){ opponent = "CopyCatBitch"; pattern = "RS"; }
+	        	if(line.contains("HappyHobo")){ opponent = "HappyHobo"; pattern = "RBBBBS"; }
+	        	if(line.contains("Jamesy")){ opponent = "Jamesy"; pattern = "RBBBS"; }
+	        	if(line.contains("MadMan")){ opponent = "MadMan"; pattern = "RRRBSSSRRRRBSSSSRRRRRRBS"; }
+	        	if(line.contains("TexasTim")){ opponent = "TexasTim"; pattern = "RBBBS"; }
+	        	if(line.contains("TieGuy")){ opponent = "Jamesy"; pattern = "RBBBS"; }
+	        	if(line.contains("Tom")){ opponent = "Jamesy"; pattern = "RBBBS"; }
+	        	if(line.contains("YellowBart")){ opponent = "YellowBart"; pattern = "RBS"; }
+	        	
 	        	
 	            sb.append(line);
 	            sb.append(System.lineSeparator());
@@ -71,8 +85,20 @@ public String play(String me, String him){
 	if(turn < pattern.length()){
 		return "" + pattern.charAt(turn);
 	}
+	if(findAmmo(me) < 6){
+	return "R";
+	}
 	return "S";
 	//System.out.println("Opponent = " + opponent);
+}
+
+int findAmmo(String hist){
+    int Ammo = 0;
+    for(char c : hist.toCharArray()){
+        if(c == 'R'){ Ammo++; }
+        else if (c == 'S'){ Ammo--;}
+    }
+    return Ammo;
 }
 
 
